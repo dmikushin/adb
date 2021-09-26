b91e3c8e75f3ef3f6536807be4178096c01e5db2
markschatza <markschatza@gmail.com>
2021-09-26 11:34:00 +0200

    WIP Fixing Windows build

---
+++ include/log/align.h
@@ -0,0 +1,36 @@
+#ifdef __cplusplus
+#ifdef _MSC_VER
+#    if (_MSC_VER >= 1800)
+#        define __alignas_is_defined 1
+#    endif
+#    if (_MSC_VER >= 1900)
+#        define __alignof_is_defined 1
+#    endif
+#else
+#    include <cstdalign>   // __alignas/of_is_defined directly from the implementation
+#endif
+#endif
+
+#ifdef __alignas_is_defined
+#    define ALIGN(X) alignas(X)
+#else
+#    ifdef __GNUG__
+#        define ALIGN(X) __attribute__ ((aligned(X)))
+#    elif defined(_MSC_VER)
+#        define ALIGN(X) __declspec(align(X))
+#    else
+#        error Unknown compiler, unknown alignment attribute!
+#    endif
+#endif
+
+#ifdef __alignof_is_defined
+#    define ALIGNOF(X) alignof(x)
+#else
+#    ifdef __GNUG__
+#        define ALIGNOF(X) __alignof__ (X)
+#    elif defined(_MSC_VER)
+#        define ALIGNOF(X) __alignof(X)
+#    else
+#        error Unknown compiler, unknown alignment attribute!
+#    endif
+#endif
